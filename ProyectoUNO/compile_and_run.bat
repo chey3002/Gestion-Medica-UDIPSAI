4747148cb317e48c4b194928ff6a2236b6f4e25b
SrChey <carlos-avb@hotmail.com>
2023-07-23 20:15:41 -0500

    Eliminación de proyectos innecesarios

--- ProyectoUNO/compile_and_run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-start /min "" cmd /c "cd /d "d:\Users\carlos\Documents\ProyectoUNO" && "C:\Users\carlo\.vscode\extensions\redhat.java-1.19.0-win32-x64\jre\17.0.7-win32-x86_64\bin\java.exe" "@C:\Users\carlo\AppData\Local\Temp\cp_44z6xre23h0ffnlh5xbpa7uf9.argfile" igu.menuPrincipal"
